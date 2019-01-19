80e43a9e8da936926061427d59ed0fa51939f47b
muhamed-hassan <mohamed.hassan.work@gmail.com>
2019-01-19 19:49:44 +0200

    Add cards game problem

---
+++ src/main/java/com/problem_solving/cards_game/CardsGameProblem.java
@@ -0,0 +1,67 @@
+package com.problem_solving.cards_game;
+
+import java.util.*;
+
+/*
+Conan Edogawa got tired of solving cases, and invited his friend, Professor Agasa, over. They decided to play a game of cards.
+Conan has n cards, and the ith card has a number ai written on it. They take turns playing, starting with Conan. In each turn, 
+the player chooses a card and removes it. Also, he removes all cards having a number strictly lesser than the number on the chosen card. 
+Formally, if the player chooses the ith card, he removes that card and removes the jth card for all j such that aj < ai.
+
+A player loses if he cannot make a move on his turn, that is, he loses if there are no cards left. Predict the outcome of the game, 
+assuming both players play optimally.
+
+Input Format:
+-------------
+The first line contains an integer n — the number of cards Conan has.
+
+The next line contains n integers a1, a2, ..., an, where ai is the number on the ith card.
+
+Constraints:
+------------
+(1 ≤ n ≤ 10^5)
+(1 ≤ ai ≤ 10^5)
+
+Output Format:
+--------------
+If Conan wins, print "Conan" (without quotes), otherwise print "Agasa" (without quotes).
+
+Input	Result
+4 5 7   Conan
+1 1     Agasa
+*/
+public class CardsGameProblem {
+    
+    public String conanOrAgasa(int[] cards) {
+
+        //make sure that (1 ≤ cards.length ≤ 10^5)
+        int numberOfCards = cards.length;
+        if (numberOfCards < 1 || numberOfCards > Math.pow(10, 5)) {
+            throw new IllegalArgumentException("The number of cards violates this rule (1 ≤ n ≤ 10^5), where n is the number of cards.");
+        }
+
+        //make sure that (1 ≤ theValueOnEachCard ≤ 105)
+        int minCardValue = Arrays.stream(cards).summaryStatistics().getMin(),
+                maxCardValue = Arrays.stream(cards).summaryStatistics().getMax();
+        long maxCardValueOccurrences = Arrays.stream(cards).filter(card -> card == Arrays.stream(cards).max().getAsInt()).count();
+        if (minCardValue < 1 || maxCardValue > 105) {
+            throw new IllegalArgumentException("An invalid card value has been found which violates this rule (1 ≤ theValueOnEachCard ≤ 105)");
+        }
+
+        boolean connanWins = false,
+                maxCardValueSelected,
+                noCardsLeft;
+        for (int cursor = 0; cursor < numberOfCards; cursor++) {
+            maxCardValueSelected = (cards[cursor] == maxCardValue) && (maxCardValueOccurrences == 1);
+            noCardsLeft = (cursor == numberOfCards - 1);
+
+            if (maxCardValueSelected || noCardsLeft) {
+                connanWins = (cursor % 2 == 0); //conan's turn -> even #=# agasa's turn -> odd
+                break;
+            }
+        }
+
+        return connanWins ? "Conan" : "Agasa";
+    }
+    
+}
---
+++ src/test/java/com/problem_solving/cards_game/CardsGameProblemTest.java
@@ -0,0 +1,38 @@
+package com.problem_solving.cards_game;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CardsGameProblemTest {
+    
+    private CardsGameProblem cardsGameProblem;
+    
+    @Before
+    public void setUp() {
+        cardsGameProblem = new CardsGameProblem();
+    }
+    
+    @Test
+    public void testConanOrAgasa_WhenConanWins() {
+        
+        int[] cards = {4, 5, 7};
+        
+        String winner = cardsGameProblem.conanOrAgasa(cards);
+        
+        assertEquals("Conan", winner);
+        
+    }
+
+    @Test
+    public void testConanOrAgasa_WhenAgasaWins() {
+        
+        int[] cards = {1, 1};
+        
+        String winner = cardsGameProblem.conanOrAgasa(cards);
+        
+        assertEquals("Agasa", winner);
+        
+    }
+    
+}
