4786fb05f807efb9bed133f2c5faeac897b499aa
muhamed-hassan <mohamed.hassan.work@gmail.com>
2019-07-01 09:48:00 +0200

    Add TODO hint to prepare the test cases of bucket filler

---
+++ src/test/java/com/problem_solving/drawing/service/BucketFillPlotterTest.java
@@ -0,0 +1,19 @@
+package com.problem_solving.drawing.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BucketFillPlotterTest {
+    
+    private FillerPlotter bucketFillPlotter;
+    
+    @Before
+    public void setUp() {
+        
+        bucketFillPlotter = new BucketFillPlotter();
+    }
+
+    //TODO - preparing various test cases
+    
+}
