74a0b869b3ff772462c9334414a4d90830c1599e
muhamed_hassan <mohamed.hassan.work@gmail.com>
2020-07-06 13:09:49 +0200

    adding 2nd element with max frequency problem

---
+++ src/main/java/com/problem_solving/second_element_with_max_frequency/SecondElementWithMaxFrequencyProblem.java
@@ -0,0 +1,26 @@
+package com.problem_solving.second_element_with_max_frequency;
+
+import static java.util.stream.Collectors.groupingBy;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SecondElementWithMaxFrequencyProblem {
+
+    public int solve(List<Integer> numbers) {
+        return numbers.stream()
+                        .collect(groupingBy(Function.identity(), Collectors.counting()))
+                        .entrySet()
+                        .stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .skip(1)
+                        .findFirst()
+                        .get()
+                        .getValue()
+                        .intValue();
+    }
+
+}
---
+++ src/test/java/com/problem_solving/second_element_with_max_frequency/SecondElementWithMaxFrequencyProblemTest.java
@@ -0,0 +1,28 @@
+package com.problem_solving.second_element_with_max_frequency;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SecondElementWithMaxFrequencyProblemTest {
+
+    private static SecondElementWithMaxFrequencyProblem secondElementWithMaxFrequencyProblem;
+
+    @BeforeAll
+    public static void setUp() {
+        secondElementWithMaxFrequencyProblem = new SecondElementWithMaxFrequencyProblem();
+    }
+
+    @Test
+    public void testSolve_WhenPassingCollectionOfIntegers_ThenReturnElementWith2ndBiggestFrequency() {
+        var numbers = List.of(1, 2, 1, 3, 4, 3, 3, 4, 9, 11, 4, 4, 4);
+
+        var actualResult = secondElementWithMaxFrequencyProblem.solve(numbers);
+
+        assertEquals(3, actualResult);
+    }
+
+}
