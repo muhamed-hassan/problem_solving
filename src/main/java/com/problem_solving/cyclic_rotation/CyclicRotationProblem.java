c2d1d3056297aab88f74ca0a49492e0d90b4eab3
muhamed-hassan <mohamed.hassan.work@gmail.com>
2020-09-02 12:30:10 +0200

    adding edge cases for CyclicRotationProblemTest

--- src/main/java/com/problem_solving/cyclic_rotation/CyclicRotationProblem.java
+++ src/main/java/com/problem_solving/cyclic_rotation/CyclicRotationProblem.java
@@ -4,7 +4,11 @@ package com.problem_solving.cyclic_rotation;
 public class CyclicRotationProblem {
 
     public int[] solution(int[] array, int shiftingTimes) {
-        int[] result = new int[array.length];
+        if (array == null || array.length == 0) throw new IllegalArgumentException("array can not be null or empty");
+
+        if (shiftingTimes < 1) throw new IllegalArgumentException("shifting times should be 1 at least");
+
+        var result = new int[array.length];
         for (var currentIndex = 0; currentIndex < array.length; currentIndex++) {
             result[(currentIndex + shiftingTimes) % array.length] = array[currentIndex];
         }
--- src/test/java/com/problem_solving/cyclic_rotation/CyclicRotationProblemTest.java
+++ src/test/java/com/problem_solving/cyclic_rotation/CyclicRotationProblemTest.java
@@ -1,9 +1,15 @@
 package com.problem_solving.cyclic_rotation;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 //https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
 public class CyclicRotationProblemTest {
@@ -19,10 +25,34 @@ public class CyclicRotationProblemTest {
     public void testSolution_WhenPassingAnArrayAndShiftingTimes_ThenReturnShiftedArrayAccordingToShiftTimes() {
         var array = new int[] { 3, 8, 9, 7, 6 };
         var shiftingTimes = 3;
+        var expectedResult = new int[] { 9, 7, 6, 3, 8 };
 
-        var shiftedArray = cyclicRotationProblem.solution(array, shiftingTimes);
+        var actualResult = cyclicRotationProblem.solution(array, shiftingTimes);
 
-        assertArrayEquals(new int[] { 9, 7, 6, 3, 8 }, shiftedArray);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+
+
+    @ParameterizedTest
+    @MethodSource("provideArgsForTestValidateCommand")
+    public void testSolution_WhenPassingInvalidParameters_ThenThrowIllegalArgumentException(int[] array, int shiftingTimes) {
+        assertThrows(IllegalArgumentException.class,
+            () -> cyclicRotationProblem.solution(array, shiftingTimes));
+    }
+
+    private static Stream<Arguments> provideArgsForTestValidateCommand() {
+        var validArray = new int[] { 3, 8, 9, 7, 6 };
+        var emptyArray = new int[0];
+        int[] nullArray = null;
+        var validShiftingTimes = 3;
+        var invalidShiftingTimes = 0;
+
+        return Stream.of(
+            Arguments.of(emptyArray, validShiftingTimes),
+            Arguments.of(nullArray, validShiftingTimes),
+            Arguments.of(validArray, invalidShiftingTimes)
+        );
     }
 
 }
