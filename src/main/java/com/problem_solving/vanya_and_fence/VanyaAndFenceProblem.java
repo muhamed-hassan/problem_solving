4195e696cff03966a8d4e5539990acdac98f7aaf
muhamed-hassan <mohamed.hassan.work@gmail.com>
2020-09-11 09:41:20 +0200

    adding edge cases of VanyaAndFenceProblemTest

--- src/main/java/com/problem_solving/vanya_and_fence/VanyaAndFenceProblem.java
+++ src/main/java/com/problem_solving/vanya_and_fence/VanyaAndFenceProblem.java
@@ -3,11 +3,17 @@ package com.problem_solving.vanya_and_fence;
 //http://codeforces.com/contest/677/problem/A
 public class VanyaAndFenceProblem {
 
-    public int findTheMinimumPossibleValidWidthOfTheRoad(int n, int h, int[] friendsHeights) {
+    public int findTheMinimumPossibleValidWidthOfTheRoad(int numberOfFriends, int heightOfFence, int[] friendsHeights) {
+        if (numberOfFriends < 1) throw new IllegalArgumentException("number of friends should be a positive number");
+
+        if (heightOfFence < 1) throw new IllegalArgumentException("height of fence should be a positive number");
+
+        if (friendsHeights == null || friendsHeights.length == 0) throw new IllegalArgumentException("friends heights array can not be null or empty");
+
         int minimumPossibleValidWidthOfTheRoad = 0;
 
-        for (var i = 0; i < n; i++) {
-            if (friendsHeights[i] <= h) {
+        for (var i = 0; i < numberOfFriends; i++) {
+            if (friendsHeights[i] <= heightOfFence) {
                 minimumPossibleValidWidthOfTheRoad++;
             } else {
                 minimumPossibleValidWidthOfTheRoad += 2;
--- src/test/java/com/problem_solving/vanya_and_fence/VanyaAndFenceProblemTest.java
+++ src/test/java/com/problem_solving/vanya_and_fence/VanyaAndFenceProblemTest.java
@@ -1,9 +1,15 @@
 package com.problem_solving.vanya_and_fence;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class VanyaAndFenceProblemTest {
 
@@ -15,13 +21,40 @@ public class VanyaAndFenceProblemTest {
     }
 
     @Test
-    public void testFindTheMinimumPossibleValidWidthOfTheRoad() {
-        int n = 6, h = 5;
-        var heights = new int[] { 7, 6, 8, 9, 10, 5 };
+    public void testFindTheMinimumPossibleValidWidthOfTheRoad_WhenParametersAreValid_ThenReturnTheCalculatedResult() {
+        int numberOfFriends = 6, heightOfFence = 5;
+        var friendsHeights = new int[] { 7, 6, 8, 9, 10, 5 };
+        var expectedResult = 11;
 
-        var result = vafp.findTheMinimumPossibleValidWidthOfTheRoad(n, h, heights);
+        var actualResult = vafp.findTheMinimumPossibleValidWidthOfTheRoad(numberOfFriends, heightOfFence, friendsHeights);
 
-        assertEquals(11, result);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgsForTestFindTheMinimumPossibleValidWidthOfTheRoadWhenParametersAreInvalid")
+    public void testFindTheMinimumPossibleValidWidthOfTheRoad_WhenParametersAreInvalid_ThenThrowIllegalArgumentException(int numberOfFriends, int heightOfFence, int[] friendsHeights) {
+        assertThrows(IllegalArgumentException.class,
+            () ->  vafp.findTheMinimumPossibleValidWidthOfTheRoad(numberOfFriends, heightOfFence, friendsHeights));
+    }
+
+    private static Stream<Arguments> provideArgsForTestFindTheMinimumPossibleValidWidthOfTheRoadWhenParametersAreInvalid() {
+        var validNumberOfFriends = 6;
+        var invalidNumberOfFriends = 0;
+
+        var validHeightOfFence = 5;
+        var invalidHeightOfFence = 0;
+
+        int[] validFriendsHeights = { 7, 6, 8, 9, 10, 5 };
+        var emptyFriendsHeights = new int[0];
+        int[] nullFriendsHeights = null;
+
+        return Stream.of(
+            Arguments.of(invalidNumberOfFriends, validHeightOfFence, validFriendsHeights),
+            Arguments.of(validNumberOfFriends, invalidHeightOfFence, validFriendsHeights),
+            Arguments.of(validNumberOfFriends, validHeightOfFence, emptyFriendsHeights),
+            Arguments.of(validNumberOfFriends, validHeightOfFence, nullFriendsHeights)
+        );
     }
 
 }
